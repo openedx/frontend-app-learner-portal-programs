2b8e2b64f18fd4769a758e14610f11753cea419c
Adam Stankiewicz <agstanki@gmail.com>
2019-04-24 07:39:41 -0400

    Email settings modal in BaseCourseCard

--- src/components/CourseCard/BaseCourseCard.jsx
+++ src/components/CourseCard/BaseCourseCard.jsx
@@ -1,69 +1,157 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { StatusAlert } from '@edx/paragon';
+
+import EmailSettingsModal from './EmailSettingsModal';
 
 import './CourseCard.scss';
 
-const BaseCourseCard = (props) => {
-  const {
-    children,
-    title,
-    startDate,
-    endDate,
-    buttonLink,
-    linkToCourse,
-    hasEmailSettings,
-    microMastersTitle,
-  } = props;
+class BaseCourseCard extends Component {
+  state = {
+    modals: {
+      emailSettings: null,
+    },
+    hasEmailsEnabled: this.props.hasEmailSettings || false,
+    hasNewEmailSettings: false,
+  };
 
-  return (
-    <div className={classNames('card mb-4', { 'is-micromasters': !!microMastersTitle })}>
-      <div className="card-body">
-        <div className="row no-gutters mb-3">
-          <div className="col-xs-12 col-md-8">
-            {microMastersTitle && (
-              <p className="font-weight-bold w-75 mb-2">
-                {microMastersTitle}
-              </p>
-            )}
-            <h3 className="card-title mb-1 font-weight-normal">
-              <a href={linkToCourse}>{title}</a>
-            </h3>
-            {startDate && (
-              <p className="card-text">
-                Course starts on {moment(startDate).format('MMMM D, YYYY')}
-              </p>
-            )}
-            {endDate && (
-              <p className="card-text">
-                Course {moment(endDate) > moment() ? 'ends' : 'ended'} on {moment(endDate).format('MMMM D, YYYY')}
-              </p>
+  setModalState = ({ key, options }) => {
+    this.setState(state => ({
+      modals: {
+        ...state.modals,
+        [key]: options,
+      },
+    }));
+  };
+
+  handleEmailSettingsModalOnClose = (hasEmailsEnabled) => {
+    this.resetModals();
+
+    if (hasEmailsEnabled !== undefined) {
+      this.setState({
+        hasNewEmailSettings: true,
+        hasEmailsEnabled,
+      });
+    }
+  };
+
+  resetModals = () => {
+    this.setState({
+      modals: {
+        emailSettings: null,
+      },
+    });
+  };
+
+  render() {
+    const {
+      modals,
+      hasEmailsEnabled,
+      hasNewEmailSettings,
+    } = this.state;
+    const {
+      children,
+      title,
+      startDate,
+      endDate,
+      buttonLink,
+      linkToCourse,
+      hasEmailSettings,
+      microMastersTitle,
+    } = this.props;
+
+    return (
+      <div className={classNames('card mb-4', { 'is-micromasters': !!microMastersTitle })}>
+        <div className="card-body">
+          <div className="row no-gutters mb-3">
+            <div className="col-xs-12 col-md-8">
+              {microMastersTitle && (
+                <p className="font-weight-bold w-75 mb-2">
+                  {microMastersTitle}
+                </p>
+              )}
+              <h3 className="card-title mb-1 font-weight-normal">
+                <a href={linkToCourse}>{title}</a>
+              </h3>
+              {startDate && (
+                <p className="card-text">
+                  Course starts on {moment(startDate).format('MMMM D, YYYY')}
+                </p>
+              )}
+              {endDate && (
+                <p className="card-text">
+                  Course {moment(endDate) > moment() ? 'ends' : 'ended'} on {moment(endDate).format('MMMM D, YYYY')}
+                </p>
+              )}
+            </div>
+            {buttonLink && (
+              <div className="col-xs-12 col-md-4 text-md-right mt-3 mt-md-0">
+                {buttonLink}
+              </div>
             )}
           </div>
-          {buttonLink && (
-            <div className="col-xs-12 col-md-4 text-md-right mt-3 mt-md-0">
-              {buttonLink}
-            </div>
+          {children && children}
+          {hasEmailSettings && (
+            <>
+              {hasNewEmailSettings &&
+                <div className="row no-gutters">
+                  <div className="col-xs-12 col-sm-10 col-md-8">
+                    <StatusAlert
+                      alertType="success"
+                      dialog={
+                        <>
+                          <FontAwesomeIcon className="mr-2" icon={['fas', 'check-circle']} />
+                          Your email settings have been saved.
+                        </>
+                      }
+                      onClose={() => {
+                        this.setState({
+                          hasNewEmailSettings: false,
+                        });
+                      }}
+                      open
+                    />
+                  </div>
+                </div>
+              }
+              <div className="row no-gutters">
+                <div className="col">
+                  <button
+                    className="btn btn-link p-0"
+                    onClick={() => {
+                      this.setModalState({
+                        key: 'emailSettings',
+                        options: {
+                          title,
+                          hasEmailsEnabled,
+                        },
+                      });
+                      this.setState({
+                        hasNewEmailSettings: false,
+                      });
+                    }}
+                  >
+                    <FontAwesomeIcon className="mr-2" icon={['fas', 'cog']} />
+                    Email settings
+                  </button>
+                  {modals.emailSettings &&
+                    <EmailSettingsModal
+                      {...modals.emailSettings}
+                      onClose={this.handleEmailSettingsModalOnClose}
+                    />
+                  }
+                </div>
+              </div>
+            </>
           )}
         </div>
-        {children && children}
-        {hasEmailSettings && (
-          <div className="row no-gutters">
-            <button
-              className="btn btn-link p-0"
-              onClick={() => { /* open course settings modal here */ }}
-            >
-              <FontAwesomeIcon className="mr-2" icon={['fas', 'cog']} />
-              Email settings
-            </button>
-          </div>
-        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 BaseCourseCard.propTypes = {
   title: PropTypes.string.isRequired,
@@ -72,6 +160,7 @@ BaseCourseCard.propTypes = {
   children: PropTypes.node,
   startDate: PropTypes.string,
   endDate: PropTypes.string,
+  hasEmailsEnabled: PropTypes.bool,
   hasEmailSettings: PropTypes.bool,
   microMastersTitle: PropTypes.string,
 };
@@ -81,6 +170,7 @@ BaseCourseCard.defaultProps = {
   children: null,
   startDate: null,
   endDate: null,
+  hasEmailsEnabled: false,
   hasEmailSettings: true,
   microMastersTitle: null,
 };
---
+++ src/components/CourseCard/EmailSettingsModal.jsx
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Input, Modal, StatusAlert } from '@edx/paragon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './EmailSettingsModal.scss';
+
+class EmailSettingsModal extends Component {
+  state = {
+    hasEmailsEnabled: this.props.hasEmailsEnabled || false,
+    isSubmitting: false,
+    isFormChanged: false,
+    error: null,
+  };
+
+  handleEmailSettingsChange = (e) => {
+    const { hasEmailsEnabled } = this.props;
+    const isChecked = e.target.checked;
+    this.setState({
+      isFormChanged: isChecked !== hasEmailsEnabled,
+      hasEmailsEnabled: isChecked,
+    });
+  };
+
+  handleSaveButtonClick = () => {
+    const { hasEmailsEnabled } = this.state;
+    // TODO: Make API POST request to set the new email settings.
+    this.setState({
+      isSubmitting: true,
+    }, () => {
+      /* This callback function simulates an API call to save the
+       * new email settings. There are lines below to either
+       * resolve (200 response) or reject the promise (error response). To
+       * see what happens when the promise is rejected, switch the comments.
+       */
+
+      // eslint-disable-next-line no-unused-vars
+      new Promise((resolve, reject) => {
+        setTimeout(resolve, 2000);
+        // setTimeout(() => reject(new Error('Network Error')), 2000);
+      })
+        .then(() => {
+          this.props.onClose(hasEmailsEnabled);
+        })
+        .catch((error) => {
+          this.setState({
+            isSubmitting: false,
+            error,
+          });
+        });
+    });
+  };
+
+  handleOnClose = () => {
+    this.setState({
+      isSubmitting: false,
+      isFormChanged: false,
+      error: null,
+    });
+    this.props.onClose();
+  };
+
+  render() {
+    const {
+      error, isFormChanged, hasEmailsEnabled, isSubmitting,
+    } = this.state;
+    const { title } = this.props;
+
+    return (
+      <>
+        <Modal
+          title={`Email Settings for ${title}`}
+          body={
+            <>
+              {error && (
+                <StatusAlert
+                  alertType="danger"
+                  dialog={
+                    <div className="d-flex">
+                      <div>
+                        <FontAwesomeIcon className="mr-3" icon={['fas', 'exclamation-triangle']} />
+                      </div>
+                      <div>
+                        An error occurred while saving your email settings. Please try again.
+                      </div>
+                    </div>
+                  }
+                  dismissible={false}
+                  open
+                />
+              )}
+              <div className="form-check">
+                <Input
+                  type="checkbox"
+                  id="email-settings"
+                  checked={hasEmailsEnabled}
+                  disabled={isSubmitting}
+                  onChange={this.handleEmailSettingsChange}
+                />
+                <label className="form-check-label ml-2" htmlFor="email-settings">
+                  Receive course emails such as reminders, schedule updates, and
+                  other critical announcements.
+                </label>
+              </div>
+            </>
+          }
+          onClose={this.handleOnClose}
+          buttons={[
+            {
+              label: (
+                <>
+                  {isSubmitting &&
+                    <FontAwesomeIcon className="mr-2" icon={['fas', 'spinner']} spin />
+                  }
+                  {isSubmitting ? 'Saving' : 'Save'}
+                  {' changes'}
+                  {isSubmitting && '...'}
+                </>
+              ),
+              buttonType: 'primary',
+              disabled: isSubmitting || !isFormChanged,
+              onClick: this.handleSaveButtonClick,
+            },
+          ]}
+          open
+        />
+      </>
+    );
+  }
+}
+
+EmailSettingsModal.propTypes = {
+  title: PropTypes.string.isRequired,
+  hasEmailsEnabled: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+
+export default EmailSettingsModal;
