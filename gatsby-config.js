8aed6774a35a42a45fde4b2382060968db07fcd5
irfanuddinahmad <irfan.ahmad@arbisoft.com>
2019-04-24 15:50:20 +0500

    added frontend-auth

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     logo: 'https://www.edx.org/sites/default/files/open-edx-logo-with-reg.png',
