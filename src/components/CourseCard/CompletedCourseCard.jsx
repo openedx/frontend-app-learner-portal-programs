9dcac0823f3530fb646907f6fe0e712a43b0cc02
Adam Stankiewicz <agstanki@gmail.com>
2019-04-25 11:43:11 -0400

    use stateful button

--- src/components/CourseCard/CompletedCourseCard.jsx
+++ src/components/CourseCard/CompletedCourseCard.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { StatefulButton } from '@edx/paragon';
 
 import BaseCourseCard from './BaseCourseCard';
 
 import './CompletedCourseCard.scss';
 
-const CompletedCourseCard = (props) => {
-  const { linkToCourse } = props;
+class CompletedCourseCard extends Component {
+  state = {
+    downloadCertificateState: 'default',
+  };
 
-  const renderButtonLink = () => (
-    <a href={linkToCourse} className="btn btn-outline-dark">
-      View Course
-    </a>
-  );
-
-  const getFinalGrade = () => {
-    const { grade: { hasPassed, numericGrade } } = props;
+  getFinalGrade = () => {
+    const { grade: { hasPassed, numericGrade } } = this.props;
     const percentGrade = Math.round(numericGrade * 100);
     return (
       <span>
@@ -26,26 +23,54 @@ const CompletedCourseCard = (props) => {
     );
   };
 
-  return (
-    <BaseCourseCard
-      buttonLink={renderButtonLink()}
-      hasEmailSettings={false}
-      {...props}
-    >
-      <div className="completed row no-gutters">
-        <div className="col-6">
-          <p className="card-text font-weight-bold">
-            Final Grade: {getFinalGrade()}
-          </p>
-          <button className="btn btn-sm btn-block btn-light text-left text-muted px-2 py-2 border">
-            <FontAwesomeIcon className="mr-2" icon="file" />
-            Download Certifcate
-          </button>
-        </div>
-      </div>
-    </BaseCourseCard>
+  renderButtonLink = () => (
+    <a href={this.props.linkToCourse} className="btn btn-outline-dark">
+      View Course
+    </a>
   );
-};
+
+  render() {
+    return (
+      <BaseCourseCard
+        buttonLink={this.renderButtonLink()}
+        hasEmailSettings={false}
+        {...this.props}
+      >
+        <div className="completed row no-gutters">
+          <div className="col-6">
+            <p className="card-text font-weight-bold">
+              Final Grade: {this.getFinalGrade()}
+            </p>
+            <StatefulButton
+              state={this.state.downloadCertificateState}
+              labels={{
+                default: 'Download Certificate',
+                pending: 'Downloading...',
+                complete: 'Download complete!',
+              }}
+              icons={{
+                default: <FontAwesomeIcon className="mr-2" icon="file" />,
+                pending: <FontAwesomeIcon className="mr-2" icon="spinner" spin />,
+                complete: <FontAwesomeIcon className="mr-2" icon="check" />,
+              }}
+              disabledStates={['pending']}
+              className="btn-sm btn-block btn-light text-muted px-2 py-2 border"
+              onClick={() => {
+                this.setState({ downloadCertificateState: 'pending' });
+                setTimeout(() => {
+                  this.setState({ downloadCertificateState: 'complete' });
+                  setTimeout(() => {
+                    this.setState({ downloadCertificateState: 'default' });
+                  }, 2000);
+                }, 2000);
+              }}
+            />
+          </div>
+        </div>
+      </BaseCourseCard>
+    );
+  }
+}
 
 CompletedCourseCard.propTypes = {
   linkToCourse: PropTypes.string.isRequired,
