07f9ab91eec2034f2157018765f107dfff63b371
irfanuddinahmad <irfan.ahmad@arbisoft.com>
2019-04-30 18:34:36 +0500

    moved Provider to Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,6 +3,8 @@ import { StaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import SiteHeader from '@edx/frontend-component-site-header';
 import SiteFooter from '@edx/frontend-component-footer';
+import { Provider } from 'react-redux';
+import store from '../../data/store';
 
 import './Layout.scss';
 
@@ -18,79 +20,81 @@ const LayoutQuery = graphql`
 `;
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={LayoutQuery}
-    render={data => (
-      <>
-        <SiteHeader
-          logo={data.site.siteMetadata.logo}
-          logoDestination="http://example.com"
-          logoAltText={data.site.siteMetadata.siteName}
-          mainMenu={[
-            {
-              type: 'item',
-              href: '#',
-              content: 'Courses',
-            },
-            {
-              type: 'item',
-              href: '#',
-              content: 'Progress',
-            },
-          ]}
-          loggedIn={false}
-          username="user"
-          avatar={null}
-          userMenu={[
-            {
-              type: 'item',
-              href: '#',
-              content: 'Dashboard',
-            },
-            {
-              type: 'item',
-              href: '#',
-              content: 'Profile',
-            },
-            {
-              type: 'item',
-              href: '#',
-              content: 'Account Settings',
-            },
-            {
-              type: 'item',
-              href: '#',
-              content: 'Logout',
-            },
-          ]}
-          loggedOutItems={[
-            { type: 'item', href: '#', content: 'Login' },
-            { type: 'item', href: '#', content: 'Sign Up' },
-          ]}
-        />
-        <>{children}</>
-        <SiteFooter
-          siteName={data.site.siteMetadata.siteName}
-          siteLogo={data.site.siteMetadata.logo}
-          marketingSiteBaseUrl="https://www.example.com"
-          supportUrl="https://www.example.com/support"
-          contactUrl="https://www.example.com/contact"
-          openSourceUrl="https://www.example.com/open"
-          termsOfServiceUrl="https://www.example.com/terms-of-service"
-          privacyPolicyUrl="https://www.example.com/privacy-policy"
-          facebookUrl="https://www.facebook.com"
-          twitterUrl="https://www.twitter.com"
-          youTubeUrl="https://www.youtube.com"
-          linkedInUrl="https://www.linkedin.com"
-          googlePlusUrl="https://plus.google.com"
-          redditUrl="https://reddit.com"
-          appleAppStoreUrl="https://store.apple.com"
-          googlePlayUrl="https://play.google.com"
-          handleAllTrackEvents={() => {}}
-        />
-      </>
-    )}
-  />
+  <Provider store={store}>
+    <StaticQuery
+      query={LayoutQuery}
+      render={data => (
+        <>
+          <SiteHeader
+            logo={data.site.siteMetadata.logo}
+            logoDestination="http://example.com"
+            logoAltText={data.site.siteMetadata.siteName}
+            mainMenu={[
+              {
+                type: 'item',
+                href: '#',
+                content: 'Courses',
+              },
+              {
+                type: 'item',
+                href: '#',
+                content: 'Progress',
+              },
+            ]}
+            loggedIn={false}
+            username="user"
+            avatar={null}
+            userMenu={[
+              {
+                type: 'item',
+                href: '#',
+                content: 'Dashboard',
+              },
+              {
+                type: 'item',
+                href: '#',
+                content: 'Profile',
+              },
+              {
+                type: 'item',
+                href: '#',
+                content: 'Account Settings',
+              },
+              {
+                type: 'item',
+                href: '#',
+                content: 'Logout',
+              },
+            ]}
+            loggedOutItems={[
+              { type: 'item', href: '#', content: 'Login' },
+              { type: 'item', href: '#', content: 'Sign Up' },
+            ]}
+          />
+          <>{children}</>
+          <SiteFooter
+            siteName={data.site.siteMetadata.siteName}
+            siteLogo={data.site.siteMetadata.logo}
+            marketingSiteBaseUrl="https://www.example.com"
+            supportUrl="https://www.example.com/support"
+            contactUrl="https://www.example.com/contact"
+            openSourceUrl="https://www.example.com/open"
+            termsOfServiceUrl="https://www.example.com/terms-of-service"
+            privacyPolicyUrl="https://www.example.com/privacy-policy"
+            facebookUrl="https://www.facebook.com"
+            twitterUrl="https://www.twitter.com"
+            youTubeUrl="https://www.youtube.com"
+            linkedInUrl="https://www.linkedin.com"
+            googlePlusUrl="https://plus.google.com"
+            redditUrl="https://reddit.com"
+            appleAppStoreUrl="https://store.apple.com"
+            googlePlayUrl="https://play.google.com"
+            handleAllTrackEvents={() => {}}
+          />
+        </>
+      )}
+    />
+  </Provider>
 );
 
 Layout.defaultProps = {
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
 import { IntlProvider } from 'react-intl';
-import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import { PrivateRoute } from '@edx/frontend-auth';
 
+import apiClient from '../data/apiClient';
+import history from '../data/history';
 import Layout from '../components/Layout/Layout';
 
-import apiClient from '../data/apiClient';
-import store from '../data/store';
-import history from '../data/history';
 
 const AppWrapper = () => (
-  <Provider store={store}>
-    <IntlProvider locale="en">
-      <Layout>
-        <ConnectedRouter history={history}>
-          <PrivateRoute
-            path="/"
-            component=""
-            authenticatedAPIClient={apiClient}
-            redirect={process.env.BASE_URL}
-          />
-        </ConnectedRouter>
-      </Layout>
-    </IntlProvider>
-  </Provider>
+  <IntlProvider locale="en">
+    <Layout>
+      <ConnectedRouter history={history}>
+        <PrivateRoute
+          path="/"
+          component=""
+          authenticatedAPIClient={apiClient}
+          redirect={process.env.BASE_URL}
+        />
+      </ConnectedRouter>
+    </Layout>
+  </IntlProvider>
 );
 
 export default AppWrapper;
