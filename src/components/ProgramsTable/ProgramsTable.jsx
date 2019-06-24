0727909ae0d0ea5a88365cdd5873fd0cd74df74b
Erina Kii <erina.kii92@gmail.com>
2019-06-24 11:17:13 -0400

    does not return masterstable for no enrollment

--- src/components/ProgramsTable/ProgramsTable.jsx
+++ src/components/ProgramsTable/ProgramsTable.jsx
@@ -24,33 +24,38 @@ class ProgramsTable extends Component {
   }
 
   componentDidMount() {
-    if (this.state.programs.length < 2) navigate(`${this.state.programs[0].slug}`);
+    if (this.state.programs.length === 1) navigate(`${this.state.programs[0].slug}`);
   }
 
   render() {
     return (
       <Layout>
-        <div className="container">
-          <h1>Program List</h1>
-          <div className="table-responsive">
-            <table className={classNames('table', 'table-sm', 'table-striped')}>
-              <thead>
-                <tr>
-                  <th>Program</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
+        {
+          this.state.programs.length
+          ?
+            <div className="container">
+              <h1>Program List</h1>
+              <div className="table-responsive">
+                <table className={classNames('table', 'table-sm', 'table-striped')}>
+                  <thead>
+                    <tr>
+                      <th>Program</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
               this.state.programs.map(program => (
                 <tr key={program.uuid}>
                   <td><a href={`${program.slug}`}>{program.name}</a></td>
                 </tr>
                   ))
           }
-              </tbody>
-            </table>
-          </div>
-        </div>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          : <div className="container">You are not enrolled in any programs</div>
+       }
       </Layout>
     );
   }
