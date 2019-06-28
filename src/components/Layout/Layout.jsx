874afe1407e3bcfba8b3e2bc942ace058439c135
Erina Kii <erina.kii92@gmail.com>
2019-06-28 12:59:23 -0400

    removed comment out

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import SiteHeader from '@edx/frontend-component-site-header';
 import SiteFooter from '@edx/frontend-component-footer';
 import { connect } from 'react-redux';
-// import { IntlProvider } from 'react-intl';
 
 
 import './Layout.scss';
