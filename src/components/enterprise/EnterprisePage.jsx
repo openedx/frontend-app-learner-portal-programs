ea031cfe6428d0b181927ba24f8619ee241bbb77
Adam Stankiewicz <agstanki@gmail.com>
2019-08-13 11:26:33 -0400

    Fix linting issue

--- src/components/enterprise/EnterprisePage.jsx
+++ src/components/enterprise/EnterprisePage.jsx
@@ -11,7 +11,9 @@ const EnterprisePage = () => (
           <div className="col-6">
             <h1>Enterprise Page</h1>
             <p>
-              Note: This page intentionally left blank and will be added to in additional PRs based on this branch. This is due to the refactoring of shared components that will come in a later PR.
+              Note: This page intentionally left blank and will be added to in additional
+              PRs based on this branch. This is due to the refactoring of shared components
+              that will come in a later PR.
             </p>
           </div>
         </div>
