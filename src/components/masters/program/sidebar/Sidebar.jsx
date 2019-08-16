324eb8a842b00e59bc47b4c280abb6b8e17ae95f
Adam Stankiewicz <agstanki@gmail.com>
2019-08-16 13:47:18 -0400

    Switch to useContext hook for functional components

--- src/components/masters/program/sidebar/Sidebar.jsx
+++ src/components/masters/program/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { sendTrackEvent } from '@edx/frontend-analytics';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,63 +7,38 @@ import { LayoutContext } from '../../../common/layout';
 import Links from './Links';
 import SidebarBlock from './SidebarBlock';
 
-class Sidebar extends Component {
-  static contextType = LayoutContext;
+const Sidebar = () => {
+  const {
+    pageContext: {
+      programDocuments, externalProgramWebsite,
+    },
+  } = useContext(LayoutContext);
 
-  render() {
-    const {
-      pageContext: {
-        programDocuments, externalProgramWebsite,
-      },
-    } = this.context;
-
-    return (
-      <>
-        {programDocuments && programDocuments.display &&
-          <SidebarBlock title={programDocuments.header} className="mb-5">
-            <Links
-              id={programDocuments.header.toLowerCase().split(' ').join('-')}
-              links={programDocuments.documents}
-              label="program documents"
-            />
-          </SidebarBlock>
-        }
-        {externalProgramWebsite && externalProgramWebsite.display &&
-          <SidebarBlock title={externalProgramWebsite.header} className="mb-5">
-            {/* eslint-disable-next-line react/no-danger */}
-            <div dangerouslySetInnerHTML={{ __html: externalProgramWebsite.description }} />
-            <p>
-              <a
-                href={externalProgramWebsite.link.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={() => {
-                  sendTrackEvent('edx.learner_portal.school_portal_link.clicked');
-                }}
-              >
-                {externalProgramWebsite.link.display_text}
-                <FontAwesomeIcon
-                  className="ml-2 text-primary"
-                  icon={faExternalLinkAlt}
-                  size="sm"
-                  aria-hidden={false}
-                  aria-label="opens in a new window"
-                />
-              </a>
-            </p>
-          </SidebarBlock>
-        }
-        <SidebarBlock title="Get Technical Support">
+  return (
+    <>
+      {programDocuments && programDocuments.display && (
+        <SidebarBlock title={programDocuments.header} className="mb-5">
+          <Links
+            id={programDocuments.header.toLowerCase().split(' ').join('-')}
+            links={programDocuments.documents}
+            label="program documents"
+          />
+        </SidebarBlock>
+      )}
+      {externalProgramWebsite && externalProgramWebsite.display && (
+        <SidebarBlock title={externalProgramWebsite.header} className="mb-5">
+          {/* eslint-disable-next-line react/no-danger */}
+          <div dangerouslySetInnerHTML={{ __html: externalProgramWebsite.description }} />
           <p>
             <a
-              href="https://support.edx.org/hc/en-us"
+              href={externalProgramWebsite.link.url}
               target="_blank"
               rel="noopener noreferrer"
               onClick={() => {
-                sendTrackEvent('edx.learner_portal.support_link.clicked');
+                sendTrackEvent('edx.learner_portal.school_portal_link.clicked');
               }}
             >
-              Go to edX help center
+              {externalProgramWebsite.link.display_text}
               <FontAwesomeIcon
                 className="ml-2 text-primary"
                 icon={faExternalLinkAlt}
@@ -74,9 +49,30 @@ class Sidebar extends Component {
             </a>
           </p>
         </SidebarBlock>
-      </>
-    );
-  }
-}
+      )}
+      <SidebarBlock title="Get Technical Support">
+        <p>
+          <a
+            href="https://support.edx.org/hc/en-us"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              sendTrackEvent('edx.learner_portal.support_link.clicked');
+            }}
+          >
+            Go to edX help center
+            <FontAwesomeIcon
+              className="ml-2 text-primary"
+              icon={faExternalLinkAlt}
+              size="sm"
+              aria-hidden={false}
+              aria-label="opens in a new window"
+            />
+          </a>
+        </p>
+      </SidebarBlock>
+    </>
+  );
+};
 
 export default Sidebar;
