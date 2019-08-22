278d72676a91ee00747da76cc9e9e8c565e2e98a
Adam Stankiewicz <agstanki@gmail.com>
2019-08-22 15:36:07 -0400

    Adding utils.js file for snake_case to camelCase object conversion

---
+++ src/common/utils.js
@@ -0,0 +1,27 @@
+import camelCase from 'lodash.camelcase';
+
+export function modifyObjectKeys(object, modify) {
+  // If the passed in object is not an object, return it.
+  if (
+    object === undefined ||
+    object === null ||
+    (typeof object !== 'object' && !Array.isArray(object))
+  ) {
+    return object;
+  }
+
+  if (Array.isArray(object)) {
+    return object.map(value => modifyObjectKeys(value, modify));
+  }
+
+  // Otherwise, process all its keys.
+  const result = {};
+  Object.entries(object).forEach(([key, value]) => {
+    result[modify(key)] = modifyObjectKeys(value, modify);
+  });
+  return result;
+}
+
+export function camelCaseObject(object) {
+  return modifyObjectKeys(object, camelCase);
+}
