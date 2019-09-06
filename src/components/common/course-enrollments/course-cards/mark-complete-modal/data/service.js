522935f5e4594ce828f7f6a3992d433f5e0499d6
Adam Stankiewicz <agstanki@gmail.com>
2019-09-06 09:08:12 -0400

    Fix eslint issue

--- src/components/common/course-enrollments/course-cards/mark-complete-modal/data/service.js
+++ src/components/common/course-enrollments/course-cards/mark-complete-modal/data/service.js
@@ -1,23 +1,22 @@
-import qs from 'query-string';
+// import qs from 'query-string';
 
 import enterpriseCourseEnrollment from './__mocks__/enterpriseCourseEnrollment.json';
 
 // eslint-disable-next-line import/prefer-default-export
 export const markCourseAsCompleteRequest = (options) => {
-  // eslint-disable-next-line no-unused-vars
-  let url = `${process.env.LMS_BASE_URL}/enterprise_learner_portal/api/v1/enterprise_course_enrollments/`;
-  if (options) {
-    url += `?${qs.stringify(options)}`;
-  }
-
+  // const endpoint = '/enterprise_learner_portal/api/v1/enterprise_course_enrollments/';
+  // let url = `${process.env.LMS_BASE_URL}${endpoint}`;
+  // if (options) {
+  //   url += `?${qs.stringify(options)}`;
+  // }
   // Temporarily mock out API response
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve) => {
     const { course_id: courseId } = options;
     const response = { ...enterpriseCourseEnrollment };
     response.data.course_run_id = courseId;
     setTimeout(() => {
-      // resolve(response);
-      reject(new Error('oops'));
+      resolve(response);
     }, 1200);
   });
+  return promise;
 };
