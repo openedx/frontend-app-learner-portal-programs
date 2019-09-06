2f6e7249b8fe2cadae7f13da784a2c0fb9bf055f
Adam Stankiewicz <agstanki@gmail.com>
2019-09-06 09:10:45 -0400

    Remove mocked out response from fetchEnterpriseCourseEnrollments

--- src/components/common/course-enrollments/data/service.js
+++ src/components/common/course-enrollments/data/service.js
@@ -1,8 +1,6 @@
 import qs from 'query-string';
 import apiClient from '../../../../apiClient';
 
-import enterpriseCourseEnrollments from './__mocks__/enterpriseCourseEnrollments.json';
-
 export const fetchProgramCourseEnrollments = (programUUID) => {
   const url = `${process.env.LMS_BASE_URL}/api/program_enrollments/v1/programs/${programUUID}/overview/`;
   return apiClient.get(url);
@@ -13,10 +11,5 @@ export const fetchEnterpriseCourseEnrollments = (enterpriseUUID) => {
     enterprise_id: enterpriseUUID,
   };
   const url = `${process.env.LMS_BASE_URL}/enterprise_learner_portal/api/v1/enterprise_course_enrollments/?${qs.stringify(queryParams)}`;
-  // return apiClient.get(url);
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ data: enterpriseCourseEnrollments });
-    }, 1200);
-  });
+  return apiClient.get(url);
 };
