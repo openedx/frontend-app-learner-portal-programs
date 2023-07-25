1c785894b2f607821e34a4ce8ea283e8b10e060f
Mashal Malik <107556986+Mashal-m@users.noreply.github.com>
2023-07-25 11:14:48 +0500

    build: update platform and implement env.config (#281)
    * build: update paltform and implement env.config
    
    * refactor: remove fallback.config file and update jest.config, env.config file
    
    * refactor: update gatsby-config file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+
+const envConfigPath = path.join(__dirname, 'env.config.js');
 module.exports = {
   transform: {
     '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
@@ -5,6 +8,7 @@ module.exports = {
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/fileTransformer.js',
+    'env.config': envConfigPath,
   },
   testPathIgnorePatterns: ['node_modules', '.cache'],
   transformIgnorePatterns: ['node_modules/(?!(gatsby|@edx/paragon|@edx/frontend-platform|@edx/frontend-component-footer-edx))'],
